fc7d28bebd49eb48a23662b3033deac20ef4cc44
ML Managuelod <mlmanaguelod@gmail.com>
2017-01-10 16:16:27 +0800

    initial commit

---
+++ countword.php
@@ -0,0 +1,21 @@
+<?php
+$str = "PHP is pretty fun!!";
+$strArray = count_chars($str,1);
+
+foreach ($strArray as $key=>$value)
+   {
+   echo "The character <b>'".chr($key)."'</b> was found $value time(s)<br>";
+   }
+?>
+
+
+<!-- <table>
+   <tr>
+       <th></th>
+       <th></th>
+       <th></th>
+       <th></th>
+       <th></th>
+   </tr>
+
+</table> -->
