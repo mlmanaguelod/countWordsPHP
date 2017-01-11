37116be159480bd3c9c9e31fe80b69e21081b792
ML Managuelod <mlmanaguelod@gmail.com>
2017-01-11 15:37:52 +0800

    initial commit

--- countword.php
+++ countword.php
@@ -36,52 +36,6 @@ print_r($count_words);
 
 
 
-<!-- <?php
-
-$contents = file_get_contents('alice.txt', 'r');
-$words = explode(' ', $contents);
-
-$words = array();
-$count_words = array_count_values($words);
-echo '<pre>';
-print_r($count_words);
-
-// print_r($words);
-
-// function array_count_values($words) {
-//         $words = array();
-//         foreach($words as $word) {
-//             foreach($word as $count_words => $value1) {
-//                 if(strtolower($count_words) == strtolower($value)) {
-//                     $word[$count_words]++;
-//                     continue 2;
-//                 }
-//             }
-//             $words[$value] = 1;
-//         }
-//         return $word;
-// }
-
-// $ar = array($word);
-// $ar2 = array_count_values($ar);
-// $ar = array_count_values($ar);
-// print_r($ar2);
-// print_r($ar);
-
-
-?> -->
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -117,30 +71,6 @@ print_r($words);
 
 
 
-
-
-
-
-
-<!-- <?php
-$word = "Hello world!!";
-$wordArray = count_chars($word,1);
-
-foreach ($wordArray as $words=>$word)
-   {
-   echo "The word <b>'".chr($words)."'</b> was found $word time(s)<br>";
-   }
-?> -->
-
-<!-- <?php
-
-// $contents = file_get_contents('alice.txt', 'r');
-
-// $words = explode(' ', $contents);
-
-$array = array("Hello","hello","kumusta","Kumusta","pig","Babuy","pig","pig","bano","Bano","bano","emz","ML","emz","ML","ML",);
-print_r(array_count_values($array));
-?> -->
 
 
 
